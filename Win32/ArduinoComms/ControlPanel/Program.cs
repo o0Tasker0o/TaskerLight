21c40e52cbe38850cc10f3c52b957818ff99e8ca
James Tasker <james_tasker1@hotmail.com>
2012-07-27 22:45:34 +0100

    Prevent more than one instance of the application from running
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- Win32/ArduinoComms/ControlPanel/Program.cs
+++ Win32/ArduinoComms/ControlPanel/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ControlPanel
 {
@@ -11,9 +12,21 @@ namespace ControlPanel
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool onlyInstance = false;
+            Mutex mutex = new Mutex(true, "TaskerLightMutex", out onlyInstance);
+
+            if(onlyInstance)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+
+                GC.KeepAlive(mutex);
+            }
+            else
+            {
+                MessageBox.Show("Application is already running", "TaskerLight", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
