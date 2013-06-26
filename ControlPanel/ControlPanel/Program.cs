7cd331d172fb2b1634f50be65dc36c5b8819a1b9
James Tasker <james_tasker1@hotmail.com>
2013-06-26 22:43:25 +0100

    Add class to connect and disconnect from a COM port
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- ControlPanel/ControlPanel/Program.cs
+++ ControlPanel/ControlPanel/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace ControlPanel
+﻿using System;
+
+namespace ControlPanel
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            SerialCommunicator serialCommunicator = new SerialCommunicator();
+
+            serialCommunicator.Connect();
+
+            Console.ReadLine();
+
+            serialCommunicator.Disconnect();
         }
     }
 }
---
+++ ControlPanel/ControlPanel/SerialCommunicator.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Ports;
+
+namespace ControlPanel
+{
+    public class SerialCommunicator
+    {
+        private SerialPort mArduinoComPort;
+
+        public SerialCommunicator()
+        {
+            mArduinoComPort = new SerialPort("COM3",
+                                             115200,
+                                             Parity.None,
+                                             8,
+                                             StopBits.One);
+        }
+
+        public void Connect()
+        {
+            mArduinoComPort.Open();
+        }
+
+        public void Disconnect()
+        {
+            mArduinoComPort.Close();
+        }
+    }
+}
