41af3ed0c5238138527c3f5a97f3895c388e0bc7
James Tasker <james_tasker1@hotmail.com>
2013-07-03 21:18:18 +0100

    Add base effect generator class
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ ControlPanel/ControlPanel/EffectGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+
+namespace ControlPanel
+{
+    internal abstract class EffectGenerator
+    {
+        private Thread mTickThread;
+        protected bool mRunning;
+        protected ColourOutputManager mOutputManager;
+
+        public EffectGenerator(ColourOutputManager outputManager)
+        {
+            mOutputManager = outputManager;
+        }
+        
+        internal void Start()
+        {
+            if(!mRunning)
+            {
+                mRunning = true;
+                mTickThread = new Thread(ThreadTick);
+                mTickThread.Start();
+            }
+        }
+
+        internal void Stop()
+        {
+            if (mRunning)
+            {
+                mRunning = false;
+                mTickThread.Join();
+            }
+        }
+
+        protected abstract void ThreadTick();
+    }
+}
