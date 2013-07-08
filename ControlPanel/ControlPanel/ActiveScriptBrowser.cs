e6e0ed7f916ce7e2d9b24b61c59da0f114ac9917
James Tasker <james_tasker1@hotmail.com>
2013-07-08 22:23:44 +0100

    Add class to list directories containing scripts
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ ControlPanel/ControlPanel/ActiveScriptBrowser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ControlPanel
+{
+    public class ActiveScriptBrowser
+    {
+        public delegate void ScriptsChangedEventHandler();
+        public event ScriptsChangedEventHandler ScriptsChanged;
+
+        FileSystemWatcher mScriptDirectoryWatcher;
+
+        protected virtual void OnScriptsChanged(object sender, FileSystemEventArgs e)
+        {
+            if (ScriptsChanged != null)
+            {
+                ScriptsChanged();
+            }
+        }
+
+        public DirectoryInfo RootDirectory
+        {
+            get
+            {
+                return new DirectoryInfo(mScriptDirectoryWatcher.Path);
+            }
+            set
+            {
+                if(null != value)
+                {
+                    mScriptDirectoryWatcher = new FileSystemWatcher(value.FullName);
+
+                    mScriptDirectoryWatcher.Changed += new FileSystemEventHandler(OnScriptsChanged);
+                    mScriptDirectoryWatcher.Created += new FileSystemEventHandler(OnScriptsChanged);
+                    mScriptDirectoryWatcher.Deleted += new FileSystemEventHandler(OnScriptsChanged);
+
+                    mScriptDirectoryWatcher.EnableRaisingEvents = true;
+                }
+            }
+        }
+
+        public List<DirectoryInfo> ScriptDirectories
+        {
+            get
+            {
+                List<DirectoryInfo> directoriesList = new List<DirectoryInfo>();
+
+                foreach(DirectoryInfo scriptDirectory in RootDirectory.GetDirectories())
+                {
+                    if(scriptDirectory.GetFiles("script.dll").Count() > 0)
+                    {
+                        directoriesList.Add(scriptDirectory);
+                    }
+                }
+
+                return directoriesList;
+            }
+        }
+
+        public ActiveScriptBrowser()
+        {
+            RootDirectory = new DirectoryInfo("./");
+        }
+    }
+}
---
+++ ControlPanel/ControlPanelTests/ActiveScriptBrowserTest.cs
@@ -0,0 +1,90 @@
+﻿using ControlPanel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ControlPanelTests
+{
+    [TestClass()]
+    public class ActiveScriptBrowserTest
+    {
+        private const String cScriptDir = "./TestScriptDir1";
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            try
+            {
+                Directory.Delete(cScriptDir, true);
+            }
+            catch(DirectoryNotFoundException)
+            {
+
+            }
+        }
+
+        [TestMethod()]
+        public void ActiveScriptBrowserListsDllDirectories()
+        {
+            ActiveScriptBrowser scriptBrowser = new ActiveScriptBrowser();
+            
+            Int32 scriptChanges = 0;
+
+            scriptBrowser.ScriptsChanged += delegate()
+            {
+                scriptChanges++;
+            };
+
+            Assert.AreEqual(new DirectoryInfo("./").FullName, scriptBrowser.RootDirectory.FullName);
+            Assert.AreEqual(0, scriptBrowser.ScriptDirectories.Count);
+            Assert.AreEqual(0, scriptChanges);
+
+            Directory.CreateDirectory(cScriptDir);
+
+            Assert.AreEqual(0, scriptBrowser.ScriptDirectories.Count);
+            System.Threading.Thread.Sleep(50);
+            Assert.AreEqual(1, scriptChanges);
+
+            using(FileStream emptyStream = File.Create(Path.Combine(cScriptDir, "script.dll"))) { }
+
+            Assert.AreEqual(1, scriptBrowser.ScriptDirectories.Count);
+            System.Threading.Thread.Sleep(50);
+            Assert.AreEqual(2, scriptChanges);
+            Assert.AreEqual(new DirectoryInfo(cScriptDir).FullName, scriptBrowser.ScriptDirectories[0].FullName);
+
+            Directory.Delete(cScriptDir, true);
+            Assert.AreEqual(0, scriptBrowser.ScriptDirectories.Count);
+            System.Threading.Thread.Sleep(50);
+            Assert.AreEqual(4, scriptChanges);  //Deleting a directory is a change plus a delete
+        }
+
+        [TestMethod()]
+        public void ActiveScriptBrowserDoesNotListDirectoriesWithOtherFiles()
+        {
+            ActiveScriptBrowser scriptBrowser = new ActiveScriptBrowser();
+
+            Directory.CreateDirectory(cScriptDir);
+
+            Assert.AreEqual(0, scriptBrowser.ScriptDirectories.Count);
+
+            using (FileStream emptyStream = File.Create(Path.Combine(cScriptDir, "script.cs"))) { }
+
+            Assert.AreEqual(0, scriptBrowser.ScriptDirectories.Count);
+
+            Directory.Delete(cScriptDir, true);
+            Assert.AreEqual(0, scriptBrowser.ScriptDirectories.Count);
+        }
+
+        [TestMethod()]
+        public void ActiveScriptBrowserRootCannotBeSetToNull()
+        {
+            ActiveScriptBrowser scriptBrowser = new ActiveScriptBrowser();
+            
+            Assert.AreEqual(new DirectoryInfo("./").FullName, scriptBrowser.RootDirectory.FullName);
+
+            scriptBrowser.RootDirectory = null;
+
+            Assert.AreEqual(new DirectoryInfo("./").FullName, scriptBrowser.RootDirectory.FullName);
+        }
+    }
+}
