4ca51832bae08912b349ddb5228d2a607f319a6b
James Tasker <james_tasker1@hotmail.com>
2013-11-14 15:53:56 +0000

    Add SettingsManager to save contrast and saturation
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ ControlPanel/ControlPanel/SettingsManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace ControlPanel
+{
+    public class SettingsManager
+    {
+        public static UInt16 OutputSaturation
+        {
+            get; set;
+        }
+
+        public static UInt16 OutputContrast
+        {
+            get; set;
+        }
+
+        public static void Save(String filename)
+        {
+            XmlDocument settingsDocument = new XmlDocument();
+
+            XmlNode rootNode = settingsDocument.AppendChild(settingsDocument.CreateElement("TaskerLightSettings"));
+
+            AddChildNode(settingsDocument, "OutputSaturation", OutputSaturation.ToString());
+            AddChildNode(settingsDocument, "OutputContrast", OutputContrast.ToString());
+
+            settingsDocument.Save(filename);
+        }
+
+        public static void Load(String filename)
+        {
+            XmlDocument settingsXml = new XmlDocument();
+
+            if(!String.IsNullOrEmpty(filename))
+            {
+                try
+                {
+                    settingsXml.Load(filename);
+                }
+                catch (FileNotFoundException)
+                {
+
+                }
+            }
+
+            OutputSaturation = UInt16.Parse(GetNodeValue(settingsXml, "OutputSaturation", "50"));
+            OutputContrast = UInt16.Parse(GetNodeValue(settingsXml, "OutputContrast", "50"));
+        }
+
+        private static void AddChildNode(XmlDocument settingsDocument, String variableName, String value)
+        {
+            XmlNode childNode = settingsDocument.CreateElement(variableName);
+            childNode.InnerText = value;
+            settingsDocument.DocumentElement.AppendChild(childNode);
+        }
+
+        private static String GetNodeValue(XmlDocument settingsDocument, String nodeName, String defaultValue)
+        {
+            XmlNode selectedNode = settingsDocument.SelectSingleNode("//TaskerLightSettings/" + nodeName);
+
+            if (null != selectedNode)
+            {
+                return selectedNode.InnerText;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
---
+++ ControlPanel/ControlPanelTests/SettingsManagerTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using ControlPanel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControlPanelTests
+{
+    [TestClass()]
+    public class SettingsManagerTest
+    {
+        const String cSettingsFilename = "./settings.xml";
+
+        [TestInitialize()]
+        public void SettingsManagerTestInitialise()
+        {
+            SettingsManager.OutputSaturation = 1;
+            SettingsManager.OutputContrast = 2;
+        }
+
+        [TestCleanup()]
+        public void SettingsManagerTestCleanup()
+        {
+            File.Delete(cSettingsFilename);
+        }
+
+        [TestMethod()]
+        public void SettingsManagerCanSaveSettingsToFile()
+        {
+            SettingsManager.Save(cSettingsFilename);
+
+            Assert.IsTrue(File.Exists(cSettingsFilename));
+
+            XmlDocument settingsXml = new XmlDocument();
+            settingsXml.Load(cSettingsFilename);
+
+            Assert.IsNotNull(settingsXml.SelectSingleNode("//TaskerLightSettings"));
+            Assert.IsNotNull(settingsXml.SelectSingleNode("//TaskerLightSettings/OutputSaturation"));
+            Assert.IsNotNull(settingsXml.SelectSingleNode("//TaskerLightSettings/OutputContrast"));
+            Assert.AreEqual("1", settingsXml.SelectSingleNode("//TaskerLightSettings/OutputSaturation").InnerText);
+            Assert.AreEqual("2", settingsXml.SelectSingleNode("//TaskerLightSettings/OutputContrast").InnerText);
+        }
+
+        [TestMethod()]
+        public void SettingsManagerCanLoadSettingsFromFile()
+        {
+            CreateTestFile(cSettingsFilename);
+
+            Assert.AreEqual(1, SettingsManager.OutputSaturation);
+            Assert.AreEqual(2, SettingsManager.OutputContrast);
+
+            SettingsManager.Load(cSettingsFilename);
+
+            Assert.AreEqual(123, SettingsManager.OutputSaturation);
+            Assert.AreEqual(321, SettingsManager.OutputContrast);
+        }
+
+        [TestMethod()]
+        public void SettingsManagerCanHandleLoadingFromNonExistantFile()
+        {
+            CreateTestFile("This file does not exist.xml");
+
+            Assert.AreEqual(1, SettingsManager.OutputSaturation);
+            Assert.AreEqual(2, SettingsManager.OutputContrast);
+
+            SettingsManager.Load(cSettingsFilename);
+
+            Assert.AreEqual(50, SettingsManager.OutputSaturation);
+            Assert.AreEqual(50, SettingsManager.OutputContrast);
+        }
+
+        [TestMethod()]
+        public void SettingsManagerCanHandleLoadingFromNullFilename()
+        {
+            Assert.AreEqual(1, SettingsManager.OutputSaturation);
+            Assert.AreEqual(2, SettingsManager.OutputContrast);
+
+            SettingsManager.Load(null);
+
+            Assert.AreEqual(50, SettingsManager.OutputSaturation);
+            Assert.AreEqual(50, SettingsManager.OutputContrast);
+        }
+
+        private static void CreateTestFile(String filename)
+        {
+            XmlDocument settingsDocument = new XmlDocument();
+
+            XmlNode rootNode = settingsDocument.AppendChild(settingsDocument.CreateElement("TaskerLightSettings"));
+
+            XmlNode outputSaturationNode = settingsDocument.CreateElement("OutputSaturation");
+            outputSaturationNode.InnerText = "123";
+            rootNode.AppendChild(outputSaturationNode);
+
+            XmlNode outputContrastNode = settingsDocument.CreateElement("OutputContrast");
+            outputContrastNode.InnerText = "321";
+            rootNode.AppendChild(outputContrastNode);
+
+            settingsDocument.Save(filename);
+        }
+    }
+}
